a4f6f3f59d73fc833753713ab616eff495b2123b
kartoone <brtoone@samford.edu>
2019-03-25 07:07:21 -0500

    updating to handle mugshots

--- src/main/java/ImagePreprocessor.java
+++ src/main/java/ImagePreprocessor.java
@@ -5,9 +5,147 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.io.*;
 import java.math.BigInteger;
+import java.nio.BufferOverflowException;
 
 public class ImagePreprocessor {
     public static void main(String[] args) throws Exception {
+//        int peoplepixels[][][] = processPeople();
+        int peoplepixels[][][] = processMugshots(-1, 28);
+        writePeople(peoplepixels);
+    }
+
+    public static void writePeople(int peoplepixels[][][]) throws IOException {
+        int peoplelength = peoplepixels.length;
+
+        // write all the cropped people out to a single file
+        // binary file ...
+        //  first byte - width of each person
+        //  second byte - height of each person
+        //  third byte - number of people in the file
+        //  all the rest of the bytes are all the people pixels in row major order
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(new File ("croppedpeople.bin")));
+        int w = peoplepixels[0].length;
+        int h = peoplepixels[0][0].length;
+        out.write(intToByteArray(w));
+        out.write(intToByteArray(h));
+        out.write(intToByteArray(peoplelength));
+        for (int pi=0; pi<peoplelength; pi++) {
+            for (int x=0; x<w; x++) {
+                for (int y=0; y<h; y++) {
+                    byte buffer[] = intToByteArray(peoplepixels[pi][x][y]);
+                    out.write(buffer);
+                }
+            }
+        }
+        out.close();
+    }
+
+    // if limit < 0, then count the number of the files in the directory and import them all
+    public static int[][][] processMugshots(int limit, int targetdim) throws IOException {
+        File f = new File("mugshots/f");
+        File mugshotfiles[] = f.listFiles();
+
+        // if limit < 0, then count the number of the files and do them all
+        if (limit<0) {
+            limit = 0;
+            for (File mf : mugshotfiles) {
+                if (!mf.isDirectory()) {
+                    limit++; // only count the files (not the directories)
+                }
+            }
+        }
+
+        // targetdim is width, height in pixels
+        int peoplepixels[][][] = new int[limit][targetdim][targetdim];
+
+        // have to use two different indices here since mugshotfiles[i] could be a directory
+        int pi = 0;
+        for (int i=0; pi<limit; i++) {
+            if (!mugshotfiles[i].isDirectory()) {
+                peoplepixels[pi] = processFile(mugshotfiles[i], targetdim);
+                BufferedImage peopleimg = new BufferedImage(targetdim, targetdim, BufferedImage.TYPE_3BYTE_BGR);
+                for (int x=0; x<targetdim; x++) {
+                    for (int y=0; y<targetdim; y++) {
+                        peopleimg.setRGB(x,y,peoplepixels[pi][x][y]);
+                    }
+                }
+                if (Math.random()<0.005)
+                    displayImage(peopleimg);
+                dumpImage(peopleimg, "mugshots/ft"+pi+".jpg");
+                pi++;
+            }
+        }
+
+        return peoplepixels;
+    }
+
+    private static int[][] processFile(File mugshotfile, int targetdim) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(new File(mugshotfile.getPath()));
+        int w = bufferedImage.getWidth();
+        int h = bufferedImage.getHeight();
+        int dim = Math.min(w,h);
+        System.out.println(w + "x" + h);
+        int rawpixels[] = new int[dim*dim];
+        int offsetx = 0;
+        int offsety = 0;
+        if (w==dim) {
+            offsety = (h-w)/2;
+        } else {
+            offsetx = (w-h)/2;
+        }
+        bufferedImage.getRGB(offsetx, offsety, dim, dim, rawpixels, 0, dim);
+
+        // create a new image with the cropped data
+        BufferedImage croppedimg = new BufferedImage(dim,dim,BufferedImage.TYPE_BYTE_GRAY);
+        for (int x=0; x<dim; x++) {
+            for (int y=0; y<dim; y++) {
+                croppedimg.setRGB(y,x,rawpixels[x*dim+y]);
+            }
+        }
+
+        // now scale the image and get the scaled pixels
+        BufferedImage scaledimg = toBufferedImage(croppedimg.getScaledInstance(targetdim, targetdim, BufferedImage.SCALE_AREA_AVERAGING));
+        int scaledpixels[] = new int[targetdim*targetdim];
+        scaledimg.getRGB(0,0,targetdim, targetdim, scaledpixels, 0, targetdim);
+
+        // finally turn the scaled pixels into 2d array (i.e., unflatten it)
+        int pixels[][] = new int[targetdim][targetdim];
+        for (int row=0; row<targetdim; row++) {
+            for (int col=0; col<targetdim; col++) {
+                int pos = row*targetdim + col;
+                pixels[col][row] = scaledpixels[pos];
+            }
+        }
+
+        return pixels;
+    }
+
+    /**
+     * Converts a given Image into a BufferedImage
+     *
+     * @param img The Image to be converted
+     * @return The converted BufferedImage
+     */
+    public static BufferedImage toBufferedImage(Image img)
+    {
+        if (img instanceof BufferedImage)
+        {
+            return (BufferedImage) img;
+        }
+
+        // Create a buffered image with transparency
+        BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+        // Draw the image on to the buffered image
+        Graphics2D bGr = bimage.createGraphics();
+        bGr.drawImage(img, 0, 0, null);
+        bGr.dispose();
+
+        // Return the buffered image
+        return bimage;
+    }
+
+    public static int[][][] processPeople() throws IOException {
         BufferedImage bufferedImage = ImageIO.read(new File("people.jpg"));
         byte[] pixels = ((DataBufferByte) bufferedImage.getRaster().getDataBuffer()).getData();
         int[] unsigned = new int[pixels.length];
@@ -22,7 +160,7 @@ public class ImagePreprocessor {
         System.out.println(h);
         System.out.println(unsigned.length);
         System.out.println(w*h*3); // note that this the same as the length of the array
-                                   // this means that for each pixel in the image, there are three bytes stored in BLUE, GREEN, RED order
+        // this means that for each pixel in the image, there are three bytes stored in BLUE, GREEN, RED order
         System.out.println(bufferedImage.getType());
         System.out.println(bufferedImage.TYPE_3BYTE_BGR); // pixel bytes are BLUE, GREEN, RED for each pixel
 
@@ -43,12 +181,11 @@ public class ImagePreprocessor {
             bufferedImage.setRGB(x, 0, newp);
         }
 
-        int peoplepixels[][][] = new int[28][28][48];
-
-        double rightx=32;
+        int peoplepixels[][][] = new int[48][28][28];
+        // determined experimentally by drawing lines using img.setRGB
+        // that code has been removed and only the result remains
         double leftx=4;
         double topy=7;
-        double bottomy=30;
         double xinc = 37.5;
         double yinc = 47.5;
 
@@ -59,13 +196,13 @@ public class ImagePreprocessor {
                 int topside = (int)Math.round(topy+peopley*yinc);
                 for (int x=leftside; x<leftside+28; x++) {
                     for (int y=topside; y<topside+28; y++) {
-                        peoplepixels[x-leftside][y-topside][peoplei] = bufferedImage.getRGB(x, y);
+                        peoplepixels[peoplei][x-leftside][y-topside] = bufferedImage.getRGB(x, y);
                     }
                 }
                 BufferedImage peopleimg = new BufferedImage(28,28,BufferedImage.TYPE_3BYTE_BGR);
                 for (int x=0; x<28; x++) {
                     for (int y=0; y<28; y++) {
-                        peopleimg.setRGB(x,y,peoplepixels[x][y][peoplei]);
+                        peopleimg.setRGB(x,y,peoplepixels[peoplei][x][y]);
                     }
                 }
                 displayImage(peopleimg);
@@ -73,81 +210,9 @@ public class ImagePreprocessor {
                 peoplei++;
             }
         }
-
-        // write all the cropped people out to a single file
-        // binary file ...
-        //  first byte - width of each person
-        //  second byte - height of each person
-        //  third byte - number of people in the file
-        //  all the rest of the bytes are all the people pixels in row major order
-        OutputStream out = new BufferedOutputStream(new FileOutputStream(new File ("croppedpeople.bin")));
-        out.write(0);
-        out.write(0);
-        out.write(0);
-        out.write(28);
-        out.write(0);
-        out.write(0);
-        out.write(0);
-        out.write(28);
-        out.write(0);
-        out.write(0);
-        out.write(0);
-        out.write(48);
-        for (int pi=0; pi<48; pi++) {
-            for (int x=0; x<28; x++) {
-                for (int y=0; y<28; y++) {
-                    byte buffer[] = intToByteArray(peoplepixels[x][y][pi]);
-                    out.write(buffer);
-                }
-            }
-        }
-        out.close();
-
-
-        // let's "find" the optimal place to crop person on the right
-        for (rightx=32; rightx<w; rightx+=37.5) {
-            for (int y = 0; y < h; y++) {
-                int cyanp = 255 << 24 | 0 << 16 | 255 << 8 | 255;
-                bufferedImage.setRGB((int)Math.round(rightx), y, cyanp);
-            }
-        }
-
-        // let's find the crop spot on the left
-        for (leftx=4; leftx<w; leftx+=37.5) {
-            for (int y = 0; y < h; y++) {
-                int cyanp = 255 << 24 | 0 << 16 | 255 << 8 | 255;
-                bufferedImage.setRGB((int)Math.round(leftx), y, cyanp);
-            }
-        }
-
-        // let's find the crop line for the top of each person
-        for (topy=7; topy<h; topy+=47.5) {
-            for (int x = 0; x < w; x++) {
-                int cyanp = 255 << 24 | 0 << 16 | 255 << 8 | 255;
-                bufferedImage.setRGB(x, (int)Math.round(topy), cyanp);
-            }
-        }
-
-        // let's find the crop line for the top of each person
-        for (bottomy=35; bottomy<h; bottomy+=47.5) {
-            for (int x = 0; x < w; x++) {
-                int cyanp = 255 << 24 | 0 << 16 | 255 << 8 | 255;
-                bufferedImage.setRGB(x, (int)Math.round(bottomy), cyanp);
-            }
-        }
-
-        displayImage(bufferedImage);
-
-//        BufferedImage bufferedImage2 = ImageIO.read(new File("people.jpg"));
-//        convertGrayscale(bufferedImage2);
-//        displayImage(bufferedImage2);
-//        byte[] pixels2 = ((DataBufferByte) bufferedImage2.getRaster().getDataBuffer()).getData();
-//        unsigned = new int[pixels2.length/3];
-//        for (int i=0; i<unsigned.length; i++) {
-//            unsigned[i] = pixels2[i*3]&255;
-//        }
-//        System.out.println(java.util.Arrays.toString(unsigned));
+        return peoplepixels;
     }
+
 
     public static final byte[] intToByteArray(int value) {
         return new byte[] {
