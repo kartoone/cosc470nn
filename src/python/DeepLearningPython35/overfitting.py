681c46e1c1bc4185c4456faead3b58042f2cf2af
kartoone <brtoone@samford.edu>
2021-04-15 10:20:18 -0500

    adding support for loading from any file

--- src/python/DeepLearningPython35/overfitting.py
+++ src/python/DeepLearningPython35/overfitting.py
@@ -13,8 +13,8 @@ import mnist_loader
 import matplotlib.pyplot as plt
 
 # uncomment these lines if you are using a mac
-#import theano
-#theano.config.gcc.cxxflags = "-Wno-c++11-narrowing"
+import theano
+theano.config.gcc.cxxflags = "-Wno-c++11-narrowing"
 
 def predictDigitNeuralNetwork(img):
     a = net.feedforward(img)
@@ -27,21 +27,26 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 validation_data = list(validation_data)
 test_data = list(test_data)
+#net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+#evaluation_cost, evaluation_accuracy, training_cost, training_accuracy = net.SGD(training_data, 50, 10, 0.5, evaluation_data=validation_data,monitor_evaluation_accuracy=True,early_stopping_n=10)
+#fig, ax = plt.subplots()
+#ax.plot(range(len(evaluation_accuracy)), [epochacc/len(validation_data) for epochacc in evaluation_accuracy])
+#ax.set_xlabel("epoch")
+#ax.set_ylabel("validation_data accuracy")
+
+
+# solution 2 to the "overfitting problem" - expand the training dataset
+expanded_training_data, validation_data, test_data = mnist_loader.load_data_wrapper("mnist_expanded.pkl.gz")
+expanded_training_data = list(expanded_training_data)
+validation_data = list(validation_data)
+test_data = list(test_data)
 net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
-evaluation_cost, evaluation_accuracy, training_cost, training_accuracy = net.SGD(training_data, 50, 10, 0.5, evaluation_data=validation_data,monitor_evaluation_accuracy=True,early_stopping_n=10)
+evaluation_cost, evaluation_accuracy, training_cost, training_accuracy = net.SGD(expanded_training_data, 50, 10, 0.5, evaluation_data=validation_data,monitor_evaluation_accuracy=True,early_stopping_n=10)
 fig, ax = plt.subplots()
 ax.plot(range(len(evaluation_accuracy)), [epochacc/len(validation_data) for epochacc in evaluation_accuracy])
 ax.set_xlabel("epoch")
 ax.set_ylabel("validation_data accuracy")
-
-exit
-
-
-# solution 2 to the "overfitting problem" - expand the training dataset
-expanded_training_data, validation_data, test_data = load_data_shared("mnist_expanded.pkl.gz")
-expanded_training_data = list(expanded_training_data)
-validation_data = list(validation_data)
-test_data = list(test_data)
+asdfasdf
 
 net = Network([
         ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28), 
